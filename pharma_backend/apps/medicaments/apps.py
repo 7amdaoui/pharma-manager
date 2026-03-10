ac361d3e6a83d2a532c03190fe99b5ca3cde17a2
7amdaoui <aminehamdaoui777@gmail.com>
2026-03-10 03:25:26 +0000

    feat: add models, migrations, API endpoints and Swagger documentation

--- pharma_backend/apps/medicaments/apps.py
+++ pharma_backend/apps/medicaments/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MedicamentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medicaments'
+    name = 'apps.medicaments'
